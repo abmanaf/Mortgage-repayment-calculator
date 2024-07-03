450ebddb556d1c5dc7f9ff4fcf5f5221f9f7a14c
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-03 14:13:36 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className="container" >
+        <div className='left-container'>
+          <div className='header' >
+            <h2 className='header'>Mortage Calculator</h2>
+            <a href="">clear All</a>
+          </div>
+          <form action="">
+            <div>
+              <label htmlFor="">Mortage Amount</label> <br />
+              <input type="text" name="mortage-amount" id="" />
+            </div>
+          <button style={{ padding: '0.8em', borderRadius: '2em', cursor: 'pointer', border: 'none', backgroundColor: 'var(--Dark-gray-blue)', color: 'var(--White)' }}>
+            Continue
+          </button>
+          </form>
+        </div>
+        <div className='right-container'>
+          <span className="product-name" style={{fontWeight: '800'}}>Summary</span>
+           
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
