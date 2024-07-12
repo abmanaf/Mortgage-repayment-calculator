fb910cbb2bcf0bdb7464b93bd93b9ecafa870a0e
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-12 00:54:44 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ function App() {
                     value={mortgageTerm}
                     onChange={(e) => setMortgageTerm(e.target.value)}
                   />
-                  <span style={{ textAlign: 'center', color: 'var(--slate-900)' }} className='icon'>Years</span>
+                   <span style={{ textAlign: 'center', color: 'var(--slate-900)' }} className='icon'>Years</span>
                 </div>
                 {error.mortgageTerm && <span className='error-message'>This field is required</span>}
               </div>
